5dd2c53b15afae491b26fd99ca0c52535d8a200e
victor kirui <vkirui@kabarak.ac.ke>
2020-06-05 13:14:33 +0300

    added tab styles updated media queries

---
+++ main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const contentItems = document.querySelectorAll('.tab-content-item');
+
+
+tabItems.forEach(item => item.addEventListener('click',toggleTabBottom));
+
+function toggleTabBottom(e){
+  //remove bottom border
+  removeBorder();
+  //remove showclass for each content item
+  removeContentItem();
+  //bottom border
+  this.classList.add('tab-border');
+  //select current item
+  const currentItem = document.querySelector(`#${this.id}-content`);
+  //add show class to the currentitem
+  currentItem.classList.add('show');
+  }
+
+function removeBorder(){
+  tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+function removeContentItem(){
+  contentItems.forEach(item => item.classList.remove('show'));
+}
+
+
